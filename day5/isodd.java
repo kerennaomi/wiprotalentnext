396491aec76d55b53a827fe03c2d5e54cdd4d0c7
kerennaomi <64479041+kerennaomi@users.noreply.github.com>
2020-06-04 09:56:14 +0530

    Add files via upload

---
+++ day5/isodd.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class isodd {
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        int n= input.nextInt();
+        if(n%2!=0)
+            System.out.println("Odd");
+    }
+}
