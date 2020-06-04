e41cb12456753f4cb4a51200f36257a00b379230
kerennaomi <64479041+kerennaomi@users.noreply.github.com>
2020-06-04 10:18:39 +0530

    Add files via upload

---
+++ day5/lastdigit.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class lastdigit {
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        int n= input.nextInt();
+        System.out.println(Math.abs(n%10));
+    }
+}
