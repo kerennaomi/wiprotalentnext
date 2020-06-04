268185fea123c83209e6d726a99f224adf2301a8
kerennaomi <64479041+kerennaomi@users.noreply.github.com>
2020-06-04 10:18:19 +0530

    Delete secondlast.java

--- day5/secondlast.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class secondlast {
-    public static void main(String[] args){
-        Scanner input= new Scanner(System.in);
-        int n= input.nextInt();
-        System.out.println((n%100)/10);
-    }
-}
